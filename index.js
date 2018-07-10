89d43ec1cf906eb87d9a5c6631b44010bc440589
Tanmay <tansbhs@yahoo.com>
2018-07-10 13:33:18 +0800

    heworhewu

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const { postgraphile } = require("postgraphile");
 
 const app = express()
 
+console.log(process.env.PORT)
+console.log(process.env.DATABASE_URL)
+
+
 app.use(postgraphile(
-  'postgres://jqpncmcxrbbfdi:56fa430948144d9d5f1a72e1c289ab36c28734bd79e96365545ab612d6bbaa8a@ec2-54-83-33-213.compute-1.amazonaws.com:5432/d5qvf40aq70u8k?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory',
+  process.env.DATABSE_URL
+  //'postgres://jqpncmcxrbbfdi:56fa430948144d9d5f1a72e1c289ab36c28734bd79e96365545ab612d6bbaa8a@ec2-54-83-33-213.compute-1.amazonaws.com:5432/d5qvf40aq70u8k?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory',
   "public",
   {
     dynamicJson: true,
@@ -24,7 +29,7 @@ app.use(bodyParser.urlencoded({
 // app.use(express.static(__dirname + '/public'));
 
 
-app.listen(process.env.port, () => {
-  console.log(`app listening on ${process.env.port}` )
+app.listen(process.env.PORT, () => {
+  console.log(`app listening on ${process.env.PORT}` )
 });
 
