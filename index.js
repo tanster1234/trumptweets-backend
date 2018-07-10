a92c73be1e0b4696a491258f26868698b59c4a9a
Tanmay <tansbhs@yahoo.com>
2018-07-10 13:36:42 +0800

    osufhodsuf

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ console.log(process.env.DATABASE_URL)
 
 
 app.use(postgraphile(
-  process.env.DATABASE_URL
+  process.env.DATABASE_URL,
   //'postgres://jqpncmcxrbbfdi:56fa430948144d9d5f1a72e1c289ab36c28734bd79e96365545ab612d6bbaa8a@ec2-54-83-33-213.compute-1.amazonaws.com:5432/d5qvf40aq70u8k?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory',
   "public",
   {
